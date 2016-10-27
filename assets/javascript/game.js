791080df83a7bcb5332ce6e52af56e440b1a6e59
Danei S <lafamilia@Jamess-MacBook-Pro.local>
2016-10-26 20:26:35 -0400

    moving images to other divs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,56 @@
-$(document).ready(function(){
+$ (document).ready(function(){
+
+	console.log("Homer");
+		$("#Homer").click(function(){
+			console.log("Homer");
+			$("#Homer").appendTo(".yourCharacter");
+			$("#Marge").appendTo(".enemAvail");
+			$("#Peter").appendTo(".enemAvail");
+			$("#Louis").appendTo(".enemAvail");
+		});
+
+		console.log("Marge");
+		$("#Marge").click(function(){
+			console.log("Marge");
+			$("#Marge").appendTo(".yourCharacter");
+			$("#Homer").appendTo(".enemAvail");
+			$("#Peter").appendTo(".enemAvail");
+			$("#Louis").appendTo(".enemAvail");
+		})
+
+		console.log("Louis");
+		$("#Louis").click(function(){
+			console.log("Louis");
+			$("#Louis").appendTo(".yourCharacter");
+			$("#Marge").appendTo(".enemAvail");
+			$("#Peter").appendTo(".enemAvail");
+			$("#Homer").appendTo(".enemAvail");
+		})
+
+		console.log("Peter");
+		$("#Peter").click(function(){
+			console.log("Peter");
+			$("#Peter").appendTo(".yourCharacter");
+			$("#Marge").appendTo(".enemAvail");
+			$("#Homer").appendTo(".enemAvail");
+			$("#Louis").appendTo(".enemAvail");
+		})
+
+
+		
+
+});
+
+
+
+
+
+
+
+	
+
+
+// ids in html use #s, and classes uses .s//
 
 
 
@@ -19,4 +71,7 @@ $(document).ready(function(){
 
 
 
-	}
+
+
+
+
